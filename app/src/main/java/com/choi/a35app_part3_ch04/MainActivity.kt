58b13dc55cd49ea366679014c11915b57a13ca56
harry <harry7408@naver.com>
2024-01-06 21:23:53 +0900

    Project Settings

---
+++ app/src/main/java/com/choi/a35app_part3_ch04/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.choi.a35app_part3_ch04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
