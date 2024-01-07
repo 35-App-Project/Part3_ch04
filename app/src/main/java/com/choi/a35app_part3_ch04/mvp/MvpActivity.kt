b3322c88fc5b44e09f8925fee8a28f82574b2a6c
harry <harry7408@naver.com>
2024-01-07 11:09:34 +0900

    feat #3 MVP 패턴으로 구현

---
+++ app/src/main/java/com/choi/a35app_part3_ch04/mvp/MvpActivity.kt
@@ -0,0 +1,52 @@
+package com.choi.a35app_part3_ch04.mvp
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import coil.load
+import com.choi.a35app_part3_ch04.databinding.ActivityMvpBinding
+import com.choi.a35app_part3_ch04.mvp.model.ImageCountModel
+import com.choi.a35app_part3_ch04.mvp.repository.ImageRepositoryImpl
+
+class MvpActivity: AppCompatActivity(), MvpContractor.View {
+
+    private lateinit var binding : ActivityMvpBinding
+
+    private lateinit var presenter: MvpContractor.Presenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMvpBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+            it.view=this
+        }
+
+        // 의존성 ( view 와 연결 )
+        presenter=MvpPresenter(ImageCountModel(),ImageRepositoryImpl())
+        presenter.attachView(this)
+    }
+
+    override fun onDestroy() {
+        presenter.detachView()
+        super.onDestroy()
+    }
+
+    fun loadImage() {
+        presenter.loadRandomImage()
+    }
+
+    override fun showImage(url: String, color: String) {
+        binding.imageView.run {
+            setBackgroundColor(Color.parseColor(color))
+            load(url) {
+                // 이미지 부드럽게 불러오기
+                crossfade(300)
+            }
+        }
+    }
+
+    override fun showImageCountText(count: Int) {
+        binding.imageCountTextView.text="불러온 이미지 : $count"
+    }
+
+}
